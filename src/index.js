21bd102737253b3f8dd641ca88ed116d1a4e36c9
chenyixin <onepiececee@163.com>
2018-01-21 20:32:25 +0800

    添加电台和热门歌单

--- src/index.js
+++ src/index.js
@@ -5,24 +5,63 @@ import Tab from './scripts/Tab.js'
 
 (function() {
 
-    let options = {
-        element:document.querySelector('#slider'),
-        sliders:[
-            {link:'#1', image:'./images/slider1.jpg'},
-            {link:'#2', image:'./images/slider2.jpg'},
-            {link:'#3', image:'./images/slider3.jpg'},
-            {link:'#4', image:'./images/slider4.jpg'},
-            {link:'#5', image:'./images/slider5.jpg'}
-        ]
+
+    fetch('./json/res.json')
+        .then(res => res.json())
+        .then(render)
+
+    function render(json){
+        renderSlider(json.data.slider)
+        renderRadio(json.data.radioList)
+        renderSong(json.data.songList)
     }
 
+    function renderSlider(sliders){
+        sliders = sliders.map(slide =>{
+             return {link:slide.linkUrl,image:slide.picUrl}
+        })
+        new Slider({
+            element : document.querySelector('#slider'),
+            sliders : sliders
+        })
+    }
+ 
+    function renderRadio(radios){
+        let radioList = document.querySelector('.radio-list');
 
-    let slider = new Slider(options)
-    window.slider = slider
 
-    let tab = new Tab()
+       radioList.innerHTML =  radios.map(radio => `
+            <li class="radio-item">
+                <a href="">
+                    <div class="item-image">
+                        <img src="${radio.picUrl}" alt="radio">
+                        <span class="icon"></span>
+                    </div>
+                    <div class="item-info">
+                        <h3>${radio.Ftitle}</h3>
+                    </div>
+                </a>
+            </li>
+        `).join('')
+    }
 
+    function renderSong(songs){
+        let hotList = document.querySelector('.hot-list');
 
+        hotList.innerHTML = songs.map(song => `
+            <li class="hot-item">
+            <a href="">
+                <div class="item-image">
+                    <img src="${song.picUrl}" alt="song">
+                    <span class="icon"></span>
+                </div>
+                <div class="item-info">
+                    <h3>${song.Ftitle}</h3>
+                </div>
+            </a>
+        </li>
+        `).join('')
+    }
 
 
 
